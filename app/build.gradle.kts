1b8794c5b9e0ce9098d501fe1fde92de8b16da68
İbrahim Eymen <ibrahimeymen@Ibrahim-MacBook-Air.local>
2026-07-13 13:05:49 +0300

    Signing config debug duzeltildi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,11 +31,9 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
+    getByName("debug"){
+
+
     }
   }
 
@@ -46,7 +44,7 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug { signingConfig = signingConfigs.getByName("debug") }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
